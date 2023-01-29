47708e6bb0abbd00fcc7637a1d26e895ca50b4ee
Sui <gujunjie52@163.com>
2023-01-29 15:41:12 +0800

    VMT update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+ 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  head: () => {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+    return <>
+      <meta property="og:title" content={frontMatter.title || 'MakiFBT'} />
+    </>
   },
+  logo: <span>MakiFBT</span>,
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/GbRGDm5R',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Deeppuma.co.ltd        Since 2021',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – MakiFBT'
+    }
   },
 }
 
